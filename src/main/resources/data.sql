c00499f30222059b56ddd03a27e321765b286f96
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-06-15 23:40:13 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO pet
 (first_name, pet_kind, gender, vaccine)
 VALUES
-('Bart','Cat','Male',true,false),
-('Lisa','Dog','Male',false,true),
-('Homer','Rabbit','Female',false,false),
-('Marge','Dog','Female',true,true);
+('Bart','Cat','Male',true),
+('Lisa','Dog','Male',false),
+('Homer','Rabbit','Female',false),
+('Marge','Dog','Female',true);
